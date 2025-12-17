1cf038a69456a61d593788268ee292f14f274892
17matb <147427088+17matb@users.noreply.github.com>
2025-12-17 12:50:38 +0100

    feat: add login page

--- apps/web/src/Login.tsx
+++ apps/web/src/Login.tsx
@@ -1,7 +1,22 @@
+import Button from './components/ui/Button';
+import TextInput from './components/ui/TextInput';
+
 const Login = () => {
   return (
-    <div>
-      <h2>Connectez-vous à votre espace professionnel</h2>
+    <div className="flex items-center justify-center min-h-dvh w-full">
+      <div className="w-full max-w-[350px] border border-dark/10 dark:border-light/10 bg-darker-light dark:bg-darker-dark p-6 squircle rounded-[4rem]">
+        <p className="text-2xl">Se connecter</p>
+        <p className="text-sm opacity-75">
+          Connectez-vous à votre espace professionnel
+        </p>
+        <form className="flex flex-col gap-2 mt-4">
+          <TextInput placeholder="Nom d'utilisateur" />
+          <TextInput placeholder="Mot de passe" />
+          <Button variant="largeClassicPrimary" className="mt-2" type="submit">
+            Connexion
+          </Button>
+        </form>
+      </div>
     </div>
   );
 };
