0c9820e8fefef104f531ddff93a996bd2b491355
17matb <147427088+17matb@users.noreply.github.com>
2026-01-25 20:34:16 +0100

    feat(api): allow all origins with CORSMiddleware

--- apps/api/src/main.py
+++ apps/api/src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.database.database import Base, engine
 from src.routes.auth_router import auth_router
@@ -7,10 +8,19 @@ from src.routes.users_router import users_router
 
 Base.metadata.create_all(bind=engine)
 
+
 app = FastAPI()
 app.include_router(users_router)
 app.include_router(auth_router)
 app.include_router(conversations_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
