5fc2d80a6e4b0aced48b2a3e3fc1d77b916f3bda
17matb <147427088+17matb@users.noreply.github.com>
2026-01-29 13:29:12 +0100

    feat(web): improve `Nav` component's styling

--- apps/web/src/components/Nav.tsx
+++ apps/web/src/components/Nav.tsx
@@ -1,4 +1,9 @@
-import { PanelLeftClose, PanelLeftOpen, type LucideIcon } from 'lucide-react';
+import {
+  ChevronsUpDown,
+  PanelLeftClose,
+  PanelLeftOpen,
+  type LucideIcon,
+} from 'lucide-react';
 import { AnimatePresence, motion } from 'motion/react';
 import { useState } from 'react';
 import Button from './ui/Button';
@@ -11,20 +16,26 @@ type Props = {
     path?: string;
   }>;
   conversations?: Array<string>;
-  adminButton: { label: string; path: string };
 };
 
-const Nav = ({ mainControls, conversations, adminButton }: Props) => {
+const Nav = ({ mainControls, conversations }: Props) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
 
   const sideBarWidth = 260;
+  const collapseDuration = 200;
 
-  const user = { first_name: 'john', last_name: 'doe' };
+  const user = {
+    first_name: 'john',
+    last_name: 'doe',
+  };
 
   return (
     <nav
-      style={{ width: isCollapsed ? '3rem' : `${sideBarWidth}px` }}
-      className="duration-200 ease-in-out shrink-0 border-r border-dark/10 dark:border-light/10 bg-darker-light dark:bg-darker-dark flex flex-col overflow-hidden"
+      style={{
+        width: isCollapsed ? '3rem' : `${sideBarWidth}px`,
+        transitionDuration: collapseDuration + 'ms',
+      }}
+      className="ease-in-out shrink-0 border-r border-dark/10 dark:border-light/10 bg-darker-light dark:bg-darker-dark flex flex-col overflow-hidden"
     >
       <div className="border-b border-dark/10 dark:border-light/10">
         <div className="flex shrink-0 justify-between items-center border-bx border-dark/10 dark:border-light/10 h-12 px-2 relative">
@@ -35,7 +46,7 @@ const Nav = ({ mainControls, conversations, adminButton }: Props) => {
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
                   exit={{ opacity: 0 }}
-                  transition={{ duration: 0.1 }}
+                  transition={{ duration: collapseDuration / 1000 }}
                   className="font-bold text-nowrap pl-2 select-none"
                 >
                   Local LLM Chat
@@ -69,6 +80,7 @@ const Nav = ({ mainControls, conversations, adminButton }: Props) => {
                       initial={{ opacity: 0 }}
                       animate={{ opacity: 1 }}
                       exit={{ opacity: 0 }}
+                      transition={{ duration: collapseDuration / 1000 }}
                       className="text-nowrap"
                     >
                       {item.label}
@@ -87,6 +99,7 @@ const Nav = ({ mainControls, conversations, adminButton }: Props) => {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
+              transition={{ duration: collapseDuration / 1000 }}
             >
               <p className="text-xs px-2 opacity-60 text-nowrap">
                 Conversations récentes
@@ -105,41 +118,31 @@ const Nav = ({ mainControls, conversations, adminButton }: Props) => {
         </AnimatePresence>
       </div>
       <div
-        className={`flex shrink-0 items-center justify-between w-[${sideBarWidth.toString()}px] gap-2 h-12 px-2 border-t border-dark/10 dark:border-light/10`}
+        className={`flex shrink-0 w-full items-center justify-between gap-1 h-12 px-2 border-t border-dark/10 dark:border-light/10 overflow-x-hidden`}
       >
-        <button className="flex items-center px-1 -ml-1 rounded-xl squircle py-1 gap-2 hover:bg-dark/10 dark:hover:bg-light/10 active:bg-dark/15 dark:active:bg-light/15 duration-100 cursor-pointer text-sm text-nowrap">
-          <div className="flex shrink-0 uppercase items-center justify-center rounded-full bg-dark dark:bg-light text-light dark:text-dark font-semibold text-sm h-8 aspect-square select-none">
+        <button className="flex w-full items-center px-1 rounded-xl squircle py-1 gap-2 hover:bg-dark/10 dark:hover:bg-light/10 active:bg-dark/15 dark:active:bg-light/15 duration-100 cursor-pointer text-sm">
+          <div className="flex shrink-0 uppercase items-center justify-center rounded-full bg-dark dark:bg-light text-light dark:text-dark font-semibold text-xs h-6 aspect-square select-none">
             {user!.first_name.charAt(0)}
             {user!.last_name.charAt(0)}
           </div>
           <AnimatePresence initial={false}>
             {!isCollapsed && (
-              <motion.p
+              <motion.div
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
+                transition={{ duration: collapseDuration / 1000 }}
+                className="flex flex-1 items-center justify-between min-w-0"
               >
-                <span className="capitalize">{user!.first_name}</span>{' '}
-                <span className="capitalize">{user!.last_name}</span>
-              </motion.p>
+                <p className="overflow-hidden text-nowrap min-w-0 truncate">
+                  <span className="capitalize">{user!.first_name}</span>{' '}
+                  <span className="capitalize">{user!.last_name[0]}.</span>
+                </p>
+                <ChevronsUpDown size={16} className="shrink-0" />
+              </motion.div>
             )}
           </AnimatePresence>
         </button>
-        <AnimatePresence initial={false}>
-          {!isCollapsed && (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-            >
-              <Link to={{ pathname: adminButton.path }}>
-                <Button variant="smallClassicPrimary">
-                  {adminButton.label}
-                </Button>
-              </Link>
-            </motion.div>
-          )}
-        </AnimatePresence>
       </div>
     </nav>
   );
