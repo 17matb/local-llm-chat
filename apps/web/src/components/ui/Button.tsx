34c1439be85aff1c32b88a2cc005a781311bf1be
17matb <147427088+17matb@users.noreply.github.com>
2026-01-31 03:01:30 +0100

    refactor: provide another idea in order to implement `Button` component

--- apps/web/src/components/ui/Button.tsx
+++ apps/web/src/components/ui/Button.tsx
@@ -4,59 +4,48 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant:
     | 'ghostIcon'
     | 'ghostText'
-    | 'smallClassicPrimary'
-    | 'largeClassicPrimary'
-    | 'largeClassicPrimaryIcon';
+    | 'smallClassicAccent'
+    | 'largeClassicAccent'
+    | 'largeClassicAccentIcon';
 }
 
 const Button = ({ variant, className, children, ...props }: ButtonProps) => {
+  const commonStyles =
+    'flex items-center shrink-0 gap-2 duration-100 cursor-pointer squircle';
+
+  let variantStyles = '';
+
   switch (variant) {
     case 'ghostIcon':
-      return (
-        <button
-          {...props}
-          className={`flex items-center justify-center shrink-0 gap-2 rounded-xl h-8 aspect-square overflow-hidden text-sm hover:bg-dark/10 dark:hover:bg-light/10 active:bg-dark/15 dark:active:bg-light/15 duration-100 cursor-pointer squircle ${className}`}
-        >
-          {children}
-        </button>
-      );
+      variantStyles =
+        'justify-center hover:bg-bg-overlay rounded-xl h-8 aspect-square overflow-hidden text-sm active:bg-bg-overlay/70';
+      break;
     case 'ghostText':
-      return (
-        <button
-          {...props}
-          className={`flex items-center justify-start shrink-0 gap-2 rounded-xl px-2 min-w-8 h-8 text-sm hover:bg-dark/10 dark:hover:bg-light/10 active:bg-dark/15 dark:active:bg-light/15 duration-100 cursor-pointer text-nowrap squircle ${className}`}
-        >
-          {children}
-        </button>
-      );
-    case 'smallClassicPrimary':
-      return (
-        <button
-          {...props}
-          className={`flex items-center justify-center bg-primary hover:bg-primary/80 active:bg-primary/70 duration-100 text-light text-sm h-8 px-2 rounded-xl cursor-pointer squircle ${className}`}
-        >
-          {children}
-        </button>
-      );
-    case 'largeClassicPrimary':
-      return (
-        <button
-          {...props}
-          className={`flex items-center justify-center bg-primary hover:bg-primary/80 active:bg-primary/70 duration-100 text-light h-12 px-4 rounded-3xl cursor-pointer squircle ${className}`}
-        >
-          {children}
-        </button>
-      );
-    case 'largeClassicPrimaryIcon':
-      return (
-        <button
-          {...props}
-          className={`flex items-center justify-center bg-primary hover:bg-primary/80 active:bg-primary/70 duration-100 text-light h-12 rounded-3xl cursor-pointer squircle ${className}`}
-        >
-          {children}
-        </button>
-      );
+      variantStyles =
+        'justify-start hover:bg-bg-overlay rounded-xl px-2 min-w-8 h-8 text-sm active:bg-bg-overlay/70 text-nowrap';
+      break;
+    case 'smallClassicAccent':
+      variantStyles =
+        'justify-center bg-accent hover:bg-accent/80 active:bg-accent/70 text-fg-on-accent text-sm h-8 px-2 rounded-xl';
+      break;
+    case 'largeClassicAccent':
+      variantStyles =
+        'justify-center bg-accent hover:bg-accent/80 active:bg-accent/70 text-fg-on-accent h-12 px-4 rounded-3xl';
+      break;
+    case 'largeClassicAccentIcon':
+      variantStyles =
+        'justify-center bg-accent hover:bg-accent/80 active:bg-accent/70 text-fg-on-accent h-12 aspect-square rounded-3xl';
+      break;
   }
+
+  return (
+    <button
+      {...props}
+      className={`${commonStyles} ${variantStyles} ${className}`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
