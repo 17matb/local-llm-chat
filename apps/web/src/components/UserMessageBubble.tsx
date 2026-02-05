2980bc8e07e3c2feafb4a16022e08070757d42e2
17matb <147427088+17matb@users.noreply.github.com>
2026-02-05 21:27:33 +0100

    feat(web): provide support for color schemes and use new `Button` component in `UserMessageBubble` component

--- apps/web/src/components/UserMessageBubble.tsx
+++ apps/web/src/components/UserMessageBubble.tsx
@@ -1,5 +1,5 @@
 import { Copy, Pencil } from 'lucide-react';
-import Button from './ui/Button';
+import { Button } from './ui/Button';
 import type { ReactNode } from 'react';
 
 type Props = {
@@ -10,14 +10,14 @@ const UserMessageBubble = ({ children }: Props) => {
   return (
     <div className="w-full flex justify-end mb-4">
       <div className="flex flex-col gap-1 max-w-[500px] items-end group">
-        <div className="bg-darker-light dark:bg-darker-dark border border-dark/10 dark:border-light/10 p-4 squircle rounded-3xl ">
+        <div className="bg-bg-surface border border-fg-subtle/10 p-4 squircle rounded-3xl ">
           {children}
         </div>
         <div className="flex">
-          <Button variant="ghostIcon">
+          <Button variant="ghost" size="sm">
             <Copy size={16} opacity={0.75} />
           </Button>
-          <Button variant="ghostIcon">
+          <Button variant="ghost" size="sm">
             <Pencil size={16} opacity={0.75} />
           </Button>
         </div>
