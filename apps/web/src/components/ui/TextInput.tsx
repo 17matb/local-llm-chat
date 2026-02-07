5c69a521437d4e59287074a6a45140862a322c5f
17matb <147427088+17matb@users.noreply.github.com>
2026-02-07 21:28:25 +0100

    feat(web): improve `TextInput` component by adding possibility to have a static icon on the left

--- apps/web/src/components/ui/TextInput.tsx
+++ apps/web/src/components/ui/TextInput.tsx
@@ -4,6 +4,7 @@ import type React from 'react';
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string;
   type?: string;
+  leftIcon?: LucideIcon;
   rightIcon?: LucideIcon;
   onRightIconClick?: () => void;
 }
@@ -11,23 +12,30 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
 const TextInput = ({
   className,
   type,
+  leftIcon: LeftIcon,
   rightIcon: RightIcon,
   onRightIconClick,
   ...props
 }: Props) => {
   return (
     <div className="relative w-full">
+      {LeftIcon && (
+        <LeftIcon
+          size={16}
+          className="absolute left-4 top-1/2 -translate-y-1/2 text-fg-subtle pointer-events-none"
+        />
+      )}
       <input
         {...props}
         type={type === 'password' ? 'password' : 'text'}
-        className={`border border-fg-subtle/10 bg-bg-surface dark:border-light/10 dark:bg-light/10 w-full h-12 px-4 squircle-md focus:outline-0 focus:border-dark/30 dark:focus:border-light/30 ${className}`}
+        className={`border border-fg-subtle/10 focus:outline-0 focus:border-fg-subtle/30 bg-bg-surface w-full h-12 px-4 squircle-md ${LeftIcon && 'pl-11'} ${RightIcon && 'pr-11'} ${className}`}
       />
       {RightIcon && (
         <button
           onClick={() => onRightIconClick}
-          className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
+          className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer"
         >
-          <RightIcon size={18} />
+          <RightIcon size={16} />
         </button>
       )}
     </div>
